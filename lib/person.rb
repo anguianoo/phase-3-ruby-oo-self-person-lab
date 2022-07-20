5ece5b8089e2538eba45bff9432d43da4973ce00
anguianoo <njanguiano@gmail.com>
2022-07-20 15:52:17 -0700

    person in progress

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # your code goes here
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+  
+  def initialize(name)
+    @bank_account = 25
+    @happiness
+    @hygiene
+  end
+  
+  
+  
+  
+end
