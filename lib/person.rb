6d5f8441fcd269882c7ff05e24f401fc5ceef8b5
anguianoo <njanguiano@gmail.com>
2022-07-20 22:24:25 -0700

    Update person.rb

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,14 @@
 class Person
   attr_accessor :bank_account
   attr_reader :name, :happiness, :hygiene
+  # def name   def happiness   def hygiene
+  #   @name      @happiness      @hygiene
+  # end        end             end
   
   def initialize(name)
     @bank_account = 25
-    @happiness
-    @hygiene
+    @happiness = 8
+    @hygiene = 8
   end
   
   
